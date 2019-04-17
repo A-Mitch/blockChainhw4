d8eac89ecf10024e4ac7dd6b82ea9bd9e5d18fb0
Alexander Mitchell <amitchellofficial@gmail.com>
2019-04-17 08:30:13 -0400

    fixes pt 2 and removes submodule

--- helloWorld/migrations/1_initial_migration.js
+++ helloWorld/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-const Migrations = artifacts.require("Migrations");
+var Migrations = artifacts.require("./Migrations.sol");
+var HelloEthSalon = artifacts.require('./HelloEthSalon.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(HelloEthSalon);
 };
